eab8c68b79c4aae49097970a0b63d21a3c487e20
Varun Irani <varunirani0505@gmail.com>
2019-10-06 17:42:23 +0530

    Grid drawing, start node, end node drawing

--- AStarPathFinding/main.cpp
+++ AStarPathFinding/main.cpp
@@ -1,91 +1,133 @@
-
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
-//
-
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+using namespace sf;
 
-// Here is a small helper for you! Have a look.
-#include "ResourcePath.hpp"
+const int WIDTH = 1400, HEIGHT = 1400;
+const int COLUMNS = 20, ROWS = 20;
+const int GRID_SQUARE_COUNT = COLUMNS * ROWS;
+const int RECT_SIZE = WIDTH / COLUMNS;
 
-int main(int, char const**)
+class Grid {
+	RectangleShape grid[ROWS][COLUMNS];
+	bool startSet, endSet;
+	RectangleShape startRect, endRect;
+public:
+	Grid() {
+		startSet = false;
+		endSet = false;
+	}
+
+	void CreateGrid() {
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				grid[i][j] = RectangleShape(Vector2f(RECT_SIZE, RECT_SIZE));
+				grid[i][j].setPosition(i * RECT_SIZE, j * RECT_SIZE);
+				grid[i][j].setFillColor(Color::White);
+				grid[i][j].setOutlineColor(Color::Black);
+				grid[i][j].setOutlineThickness(5.0);
+			}
+		}
+	}
+
+	void DrawGrid(RenderWindow &window) {
+		for (int i =0 ; i < ROWS; i++)
+			for (int j = 0; j < COLUMNS; j++)
+				window.draw(grid[i][j]);
+	}
+
+	void SetStart(RenderWindow &window) {
+		if (!startSet && Keyboard::isKeyPressed(Keyboard::S)) {
+			for (int i = 0; i < ROWS; i++) {
+				for (int j = 0; j < COLUMNS; j++) {
+					IntRect square(grid[i][j].getPosition().x, grid[i][j].getPosition().y, grid[i][j].getGlobalBounds().width - 10, grid[i][j].getGlobalBounds().height - 10);
+					if (square.contains(Mouse::getPosition(window))) {
+						grid[i][j].setFillColor(Color::Blue);
+						startSet = true;
+						startRect = grid[i][j];
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	void SetEnd(RenderWindow &window) {
+		if (!endSet && Keyboard::isKeyPressed(Keyboard::E)) {
+			for (int i = 0; i < ROWS; i++) {
+				for (int j = 0; j < COLUMNS; j++) {
+					IntRect square(grid[i][j].getPosition().x, grid[i][j].getPosition().y, grid[i][j].getGlobalBounds().width - 10, grid[i][j].getGlobalBounds().height - 10);
+					if (square.contains(Mouse::getPosition(window))) {
+						grid[i][j].setFillColor(Color::Red);
+						endSet = true;
+						endRect = grid[i][j];
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	void SetObstacle(RenderWindow &window) {
+		if (endSet && startSet && Mouse::isButtonPressed(Mouse::Left)) {
+			for (int i = 0; i < ROWS; i++) {
+				for (int j = 0; j < COLUMNS; j++) {
+					IntRect square(grid[i][j].getPosition().x, grid[i][j].getPosition().y, grid[i][j].getGlobalBounds().width - 10, grid[i][j].getGlobalBounds().height - 10);
+					bool s = (grid[i][j].getFillColor() == startRect.getFillColor()) ? true : false;
+					bool e = (grid[i][j].getFillColor() == endRect.getFillColor()) ? true : false;
+					if (square.contains(Mouse::getPosition(window))) {
+						if (!s && !e) {
+							grid[i][j].setFillColor(Color::Black);
+						}
+					}
+				}
+			}
+		}
+	}
+};
+
+int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	// Create the main window
+	RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "A* Path Finding");
 
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+	Grid *grid = new Grid();
+	grid->CreateGrid();
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+			// Escape pressed: exit
+			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+				window.close();
+			}
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
+			if (event.type == Event::MouseMoved) {
+				grid->SetObstacle(window);
+			}
 
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
+			if (event.type == Event::MouseButtonPressed) {
+				if (event.mouseButton.button == Mouse::Left) {
+					grid->SetStart(window);
+					grid->SetEnd(window);
+				}
+			}
+		}
+		// Clear screen
+		window.clear(Color::White);
 
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
+		grid->DrawGrid(window);
 
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
-        }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
-    }
-
-    return EXIT_SUCCESS;
+		// Update the window
+		window.display();
+	}
+	return EXIT_SUCCESS;
 }
